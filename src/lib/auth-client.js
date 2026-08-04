ad93c6fa4a4c855b827ebc6e948d728785df1514
ashraful-hoda <asrafulhoda789@gmail.com>
2026-08-04 11:20:30 +0600

    uri fixed

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
+    baseURL: "https://future-tech-blog-website-client.vercel.app"
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
